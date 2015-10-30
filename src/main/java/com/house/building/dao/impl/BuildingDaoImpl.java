3b48ab3cd7187d07f76b6c8f7c47ae79a962782b
hes <heshengyx@qq.com>
2015-10-30 17:14:24 +0800

    1

---
+++ src/main/java/com/house/building/dao/impl/BuildingDaoImpl.java
@@ -0,0 +1,21 @@
+package com.house.building.dao.impl;
+
+import com.house.building.dao.BaseDao;
+import com.house.building.dao.IBuildingDao;
+import com.house.building.mapper.IBuildingMapper;
+
+/**
+ * ÃèÊö£º
+ * 
+ * <pre>HISTORY
+ * ****************************************************************************
+ *  ID   DATE           PERSON          REASON
+ *  1    2015Äê10ÔÂ30ÈÕ      608279         Create
+ * ****************************************************************************
+ * </pre>
+ * @author 608279
+ * @since 1.0
+ */
+public class BuildingDaoImpl extends BaseDao<IBuildingMapper> implements IBuildingDao {
+
+}
---
+++ src/main/java/com/house/building/mapper/IBuildingMapper.java
@@ -0,0 +1,17 @@
+package com.house.building.mapper;
+
+/**
+ * ÃèÊö£º
+ * 
+ * <pre>HISTORY
+ * ****************************************************************************
+ *  ID   DATE           PERSON          REASON
+ *  1    2015Äê10ÔÂ30ÈÕ      608279         Create
+ * ****************************************************************************
+ * </pre>
+ * @author 608279
+ * @since 1.0
+ */
+public interface IBuildingMapper {
+
+}
